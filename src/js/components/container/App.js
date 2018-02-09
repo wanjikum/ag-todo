8991362c36c329fca51dfe6aabe0932e8999b7e3
wanjikum <wanjikumillicent5@gmail.com>
2018-02-10 00:24:39 +0300

    Add a create to do list form

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -4,7 +4,6 @@ class App extends React.Component{
     render(){
         return (
             <div className="container">
-                <h1>Let's do this!</h1>
                 {this.props.children}
             </div>
 
--- src/js/components/container/CreateToDo.js
+++ src/js/components/container/CreateToDo.js
@@ -2,11 +2,48 @@ import React from "react";
 import {Link} from "react-router";
 
 class CreateToDo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            toDoItem: {
+                title: ''
+            }
+        };
+        this.onToDochange = this.onToDochange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+    onToDochange(event){
+        const toDoItem = this.state.toDoItem;
+        toDoItem.title = event.target.value;
+        this.setState({toDoItem: toDoItem});
+
+    }
+
+    onClickSave(){
+        alert(`Saving ${this.state.toDoItem.title}`);
+    }
+
+
     render(){
         return (
-            <div className="jumbotron">
-                <h1>Welcome to my create to do list page</h1>
-                <Link to="editToDo" className="btn btn-primary btn-lg"> create </Link>
+            <div className="container">
+                <div className="row">
+                    <input
+                        className ="col-sm-6 input-lg"
+                        placeholder = "What do you want to do?"
+                        type="text"
+                        onChange={this.onToDochange}
+                        value={this.state.toDoItem.title}
+                    />
+                </div>
+                <div className="row">
+                    <input
+                        className = "col-sm-offset-2 col-sm-2 btn btn-lg btn-primary"
+                        type="submit"
+                        onClick={this.onClickSave}
+                        value="save"
+                    />
+                </div>
             </div>
         );
     }
