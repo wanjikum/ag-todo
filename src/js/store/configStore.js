52fb418861b0f4f2299607a7d1c1ac629258f02d
wanjikum <wanjikumillicent5@gmail.com>
2018-02-10 01:38:11 +0300

    configure the stor

---
+++ src/js/store/configStore.js
@@ -0,0 +1,12 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+
+export default function configureStore(intialState){
+    return createStore(
+        intialState,
+        rootReducer,
+        applyMiddleware(reduxImmutableStateInvariant())
+    );
+}
